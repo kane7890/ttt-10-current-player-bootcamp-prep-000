7546581a1203c02f785dae99d71e418b305d7aee
Kane Koo <kanekoo@gmail.com>
2018-11-27 21:28:36 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |boardindex| 
+    if boardindex == "X" || boardindex == "O"
+      counter = counter + 1
+    puts "element #{counter} #{boardindex}"
+    end
+  end
+  
+  return (counter)
+end
+def current_player(board)
+  turncount = turn_count(board)
+end
